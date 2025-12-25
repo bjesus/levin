87d51fa73ce06a0b545b00e6c0b94b08b128d38f
Yo'av Moshe <yoav@cloudflare.com>
2025-12-24 23:57:15 -0500

    Bump version to 0.3.3

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yoavmoshe.levin"
         minSdk = 24
         targetSdk = 34
-        versionCode = 7
-        versionName = "0.3.2"
+        versionCode = 8
+        versionName = "0.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
