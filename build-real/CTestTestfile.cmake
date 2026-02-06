a51888adbb4b6bf8ec6a2247cf724cb9532e4e33
bjesus <github@yoavmoshe.com>
2026-02-06 11:44:47 +0100

    Fix .gitignore: exclude build-real/ directory

--- build-real/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/yoav/Projects/opencode-sandbox/levin
-# Build directory: /home/yoav/Projects/opencode-sandbox/levin/build-real
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("liblevin")
